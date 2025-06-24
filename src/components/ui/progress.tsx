10bc6009fd9e95c8a2763742de05f851d8437855
Martin Hou <qolerakosmiq@gmail.com>
2025-06-24 19:30:41 +0000

    remove completely the animation of the experience progress bar in the ex

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -19,7 +19,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className={cn(
-        "h-full w-full flex-1 transition-all",
+        "h-full w-full flex-1",
         indicatorClassName ? indicatorClassName : "bg-primary"
       )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
