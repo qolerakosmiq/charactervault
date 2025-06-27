96025191893f6441bbd19d066aa97a06d40b1fe2
Martin Hou <qolerakosmiq@gmail.com>
2025-06-27 13:05:26 +0000

    base your work only on the current codebase. make textstylepanelsectionh

--- src/config/layout.ts
+++ src/config/layout.ts
@@ -20,9 +20,10 @@ export const textStyleSubLabel = "text-sm text-muted-foreground";
 export const textStyleBadgeSmall = 'text-xs';
 export const textStyleBadgeMedium = 'text-sm';
 export const textStyleSectionSubheading = 'text-lg font-semibold text-primary';
-export const textStylePanelSectionHeader = 'text-lg font-semibold text-secondary-foreground';
+export const textStylePanelSectionHeader = 'text-lg font-semibold text-muted-foreground';
 
 
 // Sizing
 export const inputWidthStandard = 'max-w-24'; // For spinners
 export const inputWidthFull = 'w-full'; // For standard inputs that should fill their container.
+
